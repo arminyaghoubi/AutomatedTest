227ced404e6e9053692e35c7dd1788f9e932d510
Armin.Yaghoubi <Armin.Yaghoubi@IT-Yaghoubi>
2023-05-02 18:35:57 +0430

    3A

---
+++ MyProject.Test/ArrangeActAssert.cs
@@ -0,0 +1,19 @@
+﻿namespace MyProject.Test;
+
+public class ArrangeActAssert
+{
+    [Fact]
+    public void Should_be_equal()
+    {
+        //Arrange
+        var firstNumber = 5;
+        var secondNumber = 7;
+        var sum = 12;
+
+        //Act
+        var result=firstNumber+ secondNumber;
+
+        //Assert
+        Assert.Equal(sum, result);
+    }
+}
